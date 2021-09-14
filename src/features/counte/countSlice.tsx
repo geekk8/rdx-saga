978c51a6bbc166adac27c8be9ef21792eb8b0778
nohkukhyun <bbi9l@naver.com>
2021-09-14 22:34:05 +0900

    count slice add

---
+++ src/features/counte/countSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const name = 'count'
+
+const initialState = {
+  number: 0,
+}
+
+const reducers = {
+  increase: (state: any) => {
+    state.number = state.number + 1
+  },
+  decrease: (state: any) => {
+    state.number = state.number - 1
+  },
+}
+
+const countSlice = createSlice({
+  name,
+  initialState,
+  reducers,
+})
+
+export const { increase, decrease } = countSlice.actions
+
+export default countSlice
